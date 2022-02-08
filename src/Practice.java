574defdb7bf825fa5356f018860c64adaec3197e
bunty <buntyraj568@gmail.com>
2022-02-08 22:18:30 +0530

    Initial commit for Java Platforms

---
+++ src/Practice.java
@@ -0,0 +1,10 @@
+public class Practice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Java Standard Edition mainly used to create application for Desktop environment"
+        +"It consist all the basic of java concept variable, primitive datatype array, Stream , Strings java database connectivity");
+        System.out.println("Java Enterprise Edition , it is use for developed web services, networking,server side Scripting");
+        System.out.println("Java Mobile Edition  it is user for mobile and small devices");
+    }
+}
